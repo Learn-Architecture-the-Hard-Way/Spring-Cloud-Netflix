95f0bf4043e239920dd030426f5cc6478af28993
JoverZhang <joverzh@gmail.com>
2020-08-13 23:47:50 +0800

    feat: part 1

---
+++ part1/p1-spring-boot-A/src/main/java/com.ttmo/springboot/SpringBootA.java
@@ -0,0 +1,19 @@
+package com.ttmo.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * TODO
+ *
+ * @author Jover Zhang
+ */
+@SpringBootApplication
+public class SpringBootA {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootA.class, args);
+        long start = System.currentTimeMillis();
+    }
+
+}
